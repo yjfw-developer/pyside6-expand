77f83ab8138c4981d5fcb8ff7a97686cbc1dc3cd
yjfw-developer <yjfw_developer@163.com>
2024-09-13 15:24:37 +0800

    update file name

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup,find_packages
 
 setup(name='pyside6-expand',
-      version='0.1',
+      version='0.2',
       description='a expand about pyside6',
-      url='',
+      url='https://github.com/yjfw-developer/pyside6-expand.git',
       author='YJFW',
+      install_requires=['PySide6'],
       packages=find_packages(),
       author_email='yjfw_developer@163.com',
       license='Apache',
